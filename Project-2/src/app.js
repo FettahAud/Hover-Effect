c8a1ad62ed9f793f9d5a9cf408ab64dfbb3e7f33
FettahAud <abooo2003adoany@gmail.com>
2021-12-09 22:47:09 +0300

    :rocket: Uploading Project-2

---
+++ Project-2/src/app.js
@@ -0,0 +1,31 @@
+let i = 1;
+const img = document.querySelector('.img');
+const select = document.querySelector('select');
+
+select.addEventListener('change', () => {
+    img.replaceChildren()
+    new hoverEffect({
+        parent: document.querySelector('.img'),
+        intensity: 0.4,
+        angle: 90,
+        image1: './src/Assets/bg1.jpg',
+        image2: './src/Assets/bg2.jpg',
+        displacementImage: `./../Displacement/dis${select.value}.jpg`,
+    });
+})
+const hover = new hoverEffect({
+    parent: document.querySelector('.img'),
+    intensity: 0.4,
+    image1: './src/Assets/bg1.jpg',
+    image2: './src/Assets/bg2.jpg',
+    displacementImage: `./../Displacement/dis${i}.jpg`,
+});
+
+// img.replaceChildren()
+// new hoverEffect({
+//     parent: document.querySelector('.img'),
+//     intensity: 0.4,
+//     image1: './src/Assets/bg1.jpg',
+//     image2: './src/Assets/bg2.jpg',
+//     displacementImage: `./../Displacement/dis${i}.jpg`,
+// });
